62c37199c2d173cfb8a64f4ce51d36053d7374f5
Skyler La <yenla@Skylers-MacBook-Pro.local>
2017-02-25 03:04:10 -0800

    Add more jquery to the game

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,49 +12,49 @@ var protagonistObj;
 var survivor = {
 	name: "survivor",
 	healthPoints: 120,
-	counterAttackPower: 12,
-	attackPower: 9
+	counterAttackPower: 13,
+	attackPower: 10
 };
 
 var ghoul = {
 	name: "ghoul",
 	healthPoints: 100,
-	counterAttackPower: 10,
-	attackPower: 8
+	counterAttackPower: 6,
+	attackPower: 10
 };
 
 var deathclaw = {
 	name: "deathclaw",
 	healthPoints: 150,
-	counterAttackPower: 16,
+	counterAttackPower: 16, 
 	attackPower: 10
 };
 
 var mutant = {
 	name: "mutant",
 	healthPoints: 180,
-	counterAttackPower: 20,
-	attackPower: 8
+	counterAttackPower: 18,
+	attackPower: 12
 }; 
 
 var enemies = [];
 var defeated = [];
 var characters = [survivor, ghoul, deathclaw, mutant];
 
-// User will pick a character
+// User will pick a character //
 
 $(document).ready(function() {
   
   	$(document).one("click", '.character_all', function(e) {
 
-  		// get the name of the div you clicked on
+  		// get the name of the div you clicked on //
   		characterSelected = $(this).attr("id");
   			$(this).appendTo("#chosen_character");
-  		// loop through characters array to see if there are variables that does NOT have the name of the div you clicked on
+  		// loop through characters array to see if there are variables that does NOT have the name of the div you clicked on //
   		for (var i = 0; i < characters.length; i++) {
 			if(characters[i].name != characterSelected) {
 
-  			// grab the divs with the name that do not match
+  			// grab the divs with the name that do not match and pushing it to the enemies array // 
   				enemies.push(characters[i].name);
 				$('#' + characters[i].name).appendTo('#enemies');
 				$('#' + characters[i].name).attr("class", "enemy_all");
@@ -65,11 +65,11 @@ $(document).ready(function() {
 			// $('#' + characters[i].name).removeClass('character_all');
 			// console.log($(`#${characters[i].name}`).attr('class'));
 
-  		} // closes for loop
+  		} // closes for loop //
 
  	});
 
-  	// user will pick an enemy then it will append to the chosen enemy div 
+  	// user will pick an enemy then it will append to the chosen enemy div //
   	$(document).on('click', '.enemy_all', function(e) {
        	enemySelected = $(this).attr("id");
 
@@ -80,7 +80,7 @@ $(document).ready(function() {
     });
 
 
-    // Setting up the attack button//
+    // Setting up the attack button //
 
         $("#attack").on("click", function() {
 	        if(isGameOver === false) {
@@ -97,8 +97,8 @@ $(document).ready(function() {
 	    	// Check for user health point and display winner or loser depend on who win //
 	    	if(protagonistObj.healthPoints <= 0) {
 	    		isGameOver = true;
-	    		hasUserLost = true;
-	    		$("instruction").text("Game Over!");
+	    		// hasUserLost = true;
+	    		$("#instruction").text("Game Over!");
 	    		$("#play_again").text("Try Again");
 	    		$('#' + characterSelected + "HP"). text("Defeated");
 	    		$('#' + enemySelected + "HP").text("Winner");
@@ -107,19 +107,25 @@ $(document).ready(function() {
 	    		});
 	    	}
 
-	    	// Check to see if the user deafeated the enemy // 
+	    	// Check to see if the user defeated the enemy // 
 
-	    	if(enemyObj.healthPoints <= 0 && hasUserLost === false) {
-	    		$("#instruction").text("You deated " + enemyObj.name + "Choose a new enemy from the list!");
-	    		$('#' + enemyObj.name).appendTo("#defeated");
-	    		$('#' + enemyObj.name + "HP").text("Defeated");
-	    		$("#attack").css("display", "none");
+	    	if(enemyObj.healthPoints <= 0) {
+	    		$("#instruction").text("You defeated " + enemyObj.name + "." + " Choose a new enemy from the list!");
+	    		$('#' + enemyObj.name).hide();
 	    		defeated.push(enemyObj);
 
 	    	}
 
 	    	// Check to see if all the enemies are defeated //
-
-    });
+	    	if(defeated.length === characters.length -1){
+	    		isGameOver = true;
+	    		$("#instruction").text("You Win!!! and have defeated all enemy");
+	    		$("#play_again").text("Play Again");
+	    		$("#" + protagonistObj.name + "HP").text("Winner!!!");
+	    		$("#play_again").on("click", function() {
+	    			location.reload();
+	    		});
+    		}
+    	});
 
 }); 
