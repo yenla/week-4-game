f9e189999dbb1af3a34a5246c9916273e260dfc7
Skyler La <yenla@Skylers-MacBook-Pro.local>
2017-02-24 19:58:59 -0800

    Add on click for choosing an enemy

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,15 @@
 
-var enemySelected;
-// var characterSelected;
-var isGameover;
+var isGameover = false;
 var hasUserLost;
+
+var characterSelected;
+var enemySelected;
+
 
 var survivor = {
 	name: "survivor",
 	healthPoints: 120,
-	// baseAttackPower: 8,
+	baseAttackPower: 8,
 	counterAttackPower: 10,
 	attackPower: 8
 };
@@ -15,7 +17,7 @@ var survivor = {
 var ghoul = {
 	name: "ghoul",
 	healthPoints: 100,
-	// baseAttackPower: 10,
+	baseAttackPower: 10,
 	counterAttackPower: 15,
 	attackPower: 10
 };
@@ -23,7 +25,7 @@ var ghoul = {
 var deathclaw = {
 	name: "deathclaw",
 	healthPoints: 150,
-	// baseAttackPower: 12,
+	baseAttackPower:  12,
 	counterAttackPower: 16,
 	attackPower: 12
 };
@@ -31,7 +33,7 @@ var deathclaw = {
 var mutant = {
 	name: "mutant",
 	healthPoints: 180,
-	// baseAttackPower: 8,
+	baseAttackPower: 8,
 	counterAttackPower: 20,
 	attackPower: 8
 }; 
@@ -40,21 +42,53 @@ var enemies = [];
 var defeated = [];
 var characters = [survivor, ghoul, deathclaw, mutant];
 
+// 1a. User will pick a character
+
 $(document).ready(function() {
-  // 1a. User will pick a character
-  	$(".character_all").on("click", function(characterSelected) {
-  		($(this).appendTo("#chosen_character"));
-  		// $(this).appendTo("#chosen_character");
-  		for (var i = 0; i < characters.length; i++){
-  			if(characters[i] != characterSelected){
-  				enemies.push(characters[i]);
-  				$(characters[i].name).appendTo("#enemies");
+  
+  	$(document).one("click", '.character_all', function(e) {
 
-  			}
-  		}
+  		// get the name of the div you clicked on
+  		characterSelected = $(this).attr("id");
+  			$(this).appendTo("#chosen_character");
+  		// loop through characters array to see if there are variables that does NOT have the name of the div you clicked on
+  		for (var i = 0; i < characters.length; i++) {
+			if(characters[i].name != characterSelected) {
+
+  			// grab the divs with the name that do not match
+  				enemies.push(characters[i].name);
+				$('#' + characters[i].name).appendTo('#enemies');
+				$('#' + characters[i].name).attr("class", "enemy_all");
+
+			}	
+
+			$("#instruction").text("Choose an enemy to battle from the list!");
+			// $('#' + characters[i].name).removeClass('character_all');
+			// console.log($(`#${characters[i].name}`).attr('class'));
+
+  		} // closes for loop
+
  	});
+  	// user will pick an enemy then it will append to the chosen enemy div 
+  	$(document).one('click', '.enemy_all', function(e) {
+       	enemySelected = $(this).attr("id");
+       	
+       	// console.log(enemySelected);
+       	$('#' + enemySelected).appendTo('#chosen_enemy');
+       	$("#instruction").text("Lets Fight! Hit the attack button to attack " + enemySelected.name);
+    });
 
-});
+    $("#attack").on("click", function() { 
+    	if(isGameover === false) {
+    		enemySelected.healthPoints -= characterSelected.attackPower;
+    		characterSelected.healthPoints -= enemySelected.counterAttackPower;
+    		$('#' + characterSelected.name + "HP").html("HP: " + characterSelected.healthPoints);
+    		$('#' + enemySelected.name + "HP").html("HP: " + enemySelected.healthPoints);
+    	}
+
+    });
+
+}); 
 
   // 1b. The character will be move to the chosen character section
   // 1c. Other remaining character will be move to the enemy section
@@ -63,11 +97,4 @@ $(document).ready(function() {
   // 4. The attack function will repeat until the enemy is death and then will loop back to choosing a new enemy
   // 5. Check to see if user lost 
   // 6. Check to see if the enemy lost and user defeate all enemies
-
-// $(document).ready(function() {
-//  $(".select").on("click", function() {
-//  $(".select").appendTo("#enemy");
-// 	$(this).appendTo("#appendTo");
-//  });
-// });
 
